e395b0c929cd46f881c95951b168fa616ccb736f
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-08-07 17:47:27 -0300

    .

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -3,6 +3,12 @@ set -e -x
 
 ### Load env
 cd git-pr
+if [ $? != 0 ];
+then
+    echo "Error en cd";
+    exit 243;
+fi
+
 
 yes admin | cf login -a https://api.apps.64.78.155.215.nip.io -o system --skip-ssl-validation
 if [ $? != 0 ];
