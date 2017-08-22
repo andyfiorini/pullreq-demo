c6472b79ea6f8583005cbf0f88f97cc89b327b75
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-08-22 15:49:34 -0300

    ,2

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -3,5 +3,8 @@ set -ex
 
 cd git-pr/
 ./gradlew clean assemble
+echo "************************  Build ok  *******************"
+ls -ltr manifest* && cat manifest.yml
+echo "************************  Build ok  *******************"
 cd -
 
