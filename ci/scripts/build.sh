3637c403f47053abd9fde5a43b8cccebc3e95b26
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-08-23 17:18:57 -0300

    .

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -8,5 +8,5 @@ ls -ltr manifest* && cat manifest.yml
 echo "************************  Build ok  *******************"
 cd -
 tar cvfz /tmp/git-pr.tar.gz git-pr
-ls -ltr $PWD/git-pr.tar.gz /tmp/git-pr.tar.gz
+#ls -ltr $PWD/git-pr.tar.gz /tmp/git-pr.tar.gz
 
