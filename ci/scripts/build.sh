b0719b869c8e83eebad6554643212dce5cb10883
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-08-24 10:29:38 -0300

    .

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
-#set -ex
+set -ex
 
 cd git-pr/
 ./gradlew clean assemble
 echo "************************  Build ok  *******************"
-ls -ltr manifest* && cat manifest.yml
 echo "************************  Build ok  *******************"
+FORMER_APP_NAME=`grep "\- name: " manifest.yml | awk -F: '{print $2}'`
+TIMESTAMP=`date +%s`
+NEW_APP_NAME=`printf ("%stest%s", $APP_NAME, $TIMESTAMP );`
+cat manifest.yml | sed '/\- name:/d' > tmp1
+echo "\- name: $NEW_APP_NAME" >> tmp1 
+mv tmp1 manifest.yml
+echo "************************  Manifest mod ok  *******************"
+echo "NEW APP NAME IS: $NEW_APP_NAME
+ls -ltr manifest* && cat manifest.yml
+echo "************************                   *******************"
 cd -
 tar cvfz outputs/git-pr-1.0.0.tar.gz git-pr
 #ls -ltr $PWD/git-pr.tar.gz /tmp/git-pr.tar.gz
