e93d990f8614196f84d0250e5bc8d0633324826b
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-09-08 11:41:00 -0300

    .

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -18,4 +18,4 @@ export APP_NAME=$(cat push.log | grep "Starting app " | awk '{print $3}')
 
 cd -
 
-echo "APP_URL=$APP_URL\nAPP_NAME=$APP_NAME\n" > outputs/app_data_$APP_NAME.txt
+echo "APP_URL=$APP_URL\nAPP_NAME=$APP_NAME\n" > outputs/app_data_1.$APP_NAME.txt
