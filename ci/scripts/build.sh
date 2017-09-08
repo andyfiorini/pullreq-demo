956b11cd59be1e06b8f235c110445fa850eb28d2
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-09-08 10:15:39 -0300

    .

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash 
-set -e
+set -e 
 
 cd pull-request/
 ./gradlew clean assemble
-echo "************************  Build ok  *******************"
-echo "************************  Build ok  *******************"
+if [ $? != 0 ];
+then
+  echo "BUILD CON ERROR."
+  exit 254;
+else 
+  echo "************************  Build ok  *******************"
+fi
 FORMER_APP_NAME=`grep "\- name: " manifest.yml | awk -F: '{print $2}'` 
 TIMESTAMP=`date +%s`
 NEW_APP_NAME=`printf "%stest%s" $APP_NAME $TIMESTAMP ;`
+echo "************************  Manifest mod ok  *******************"
+echo "NEW APP NAME IS: $NEW_APP_NAME"
+echo "************************                   *******************"
 cat manifest.yml | sed '/- name:/d' > tmp1
 cat tmp1 | sed '/^$/d' > tmp2
 head -2 manifest.yml > header
 LINES=$(wc -l manifest.yml| awk '{print $1}')
 let LINESm2=$((LINES))-2;
 tail -$LINESm2 manifest.yml > footer
-cat footer | grep -v "- name:" | grep -v "  host: " > end
+cat footer | grep -v "\- name:" | grep -v "host:" > end
 echo "- name: $NEW_APP_NAME" > tmp2 
 echo "  host: $NEW_APP_NAME" >> tmp2 
 echo "************** NEW MANIFEST *******************"
 cat header tmp2 end | tee manifest.yml
 echo "***********************************************"
-echo "************************  Manifest mod ok  *******************"
-echo "NEW APP NAME IS: $NEW_APP_NAME"
-ls -ltr manifest* && cat manifest.yml
-echo "************************                   *******************"
 cd -
 tar cvfz outputs/git-pr-1.$TIMESTAMP.tar.gz pull-request
-#ls -ltr $PWD/git-pr.tar.gz /tmp/git-pr.tar.gz
 
