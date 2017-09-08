45ed2f2af891b412033ecc7c0bd76e55ee73e50a
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-09-08 11:06:32 -0300

    .

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -9,7 +9,7 @@ chdir pull-request
 [ $? != 0 ] && (echo "wrong chdir... $?" && exit 254);
 cf login -a $cfapi -u $cfcred -p $cfsecret --skip-ssl-validation -o $cforg -s $cfspace 
 [ $? != 0 ] && (echo "cf login... $?" && exit 253);
-cf push $app-name -f $app-man-name | tee push.log ; 
+cf push $appname -f $appmanname | tee push.log ; 
 [ $? != 0 ] && (echo "cf push... $?" && exit 252);
 
 APP_URL=$(cat push.log | grep "urls: " | awk '{print $2}')
