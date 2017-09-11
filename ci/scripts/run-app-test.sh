5268a4dcd7332beef1eeffe5fbd999edc743ddff
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-09-11 10:02:21 -0300

    modfied readme

--- ci/scripts/run-app-test.sh
+++ ci/scripts/run-app-test.sh
@@ -4,6 +4,6 @@
 cd app-data
 find .; 
 set +x ; 
-APP_URL=$(cat app_data*.txt | grep URL | awk -F\= "{print $2}") 
+APP_URL=$(cat app_data*.txt | grep URL | awk -F\= "{print $2}" |  awk -F\= "{print $2}")
 curl -vvv -k $APP_URL | tee ../outputs/test-data-1.0.txt
 
