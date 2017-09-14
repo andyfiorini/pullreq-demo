d434d990f24e6f1451b882326d5bb76e06c5e881
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-09-14 12:38:53 -0300

    1505403533

--- ci/scripts/run-app-test.sh
+++ ci/scripts/run-app-test.sh
@@ -6,4 +6,5 @@ find .;
 set +x ; 
 APP_URL=`cat appdata*.txt | grep URL | awk -F= '{print $2}' `;
 curl -vvv -k $APP_URL | tee ../outputs/test-data-1.0.0.txt
+[ $? = 0 ] && echo "****************** OK ********************" 
 
--- ci/scripts/run-doc-gen.sh
+++ ci/scripts/run-doc-gen.sh
@@ -2,3 +2,10 @@
 #
 #
 env > outputs/README-1.0.md
+if grep "** OK **" test-data/*.txt;
+then
+    echo "TEST OK" | tee -a outputs/README\-1.0.md
+else
+    echo "TEST FAILURE" | tee -a outputs/README\-1.0.md
+    exit 250;
+fi
