f435d2826427e1dc877ad17148088b6d6ef6a091
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-09-14 13:53:38 -0300

    1505408018

--- ci/scripts/run-app-test.sh
+++ ci/scripts/run-app-test.sh
@@ -6,5 +6,8 @@ find .;
 set +x ; 
 APP_URL=`cat appdata*.txt | grep URL | awk -F= '{print $2}' `;
 curl -vvv -k $APP_URL | tee ../outputs/test-data-1.0.0.txt
-[ $? = 0 ] && echo "****************** OK ********************"  | tee -a ../outputs/test-data-1.0.0.txt
+[ $? = 0 ] && echo "****************** TEST OK ********************"  | tee -a ../outputs/test-data-1.0.0.txt
+env > ../outputs/README.md
+cd ../outputs
+tar cvfz test-data-1.0.0.tar test-data-1.0.0.txt README.md
 
