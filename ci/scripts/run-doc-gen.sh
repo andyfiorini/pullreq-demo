4b928d0d3c561169bd6398dd3a9f9bd90f621e21
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-09-14 13:38:26 -0300

    1505407106

--- ci/scripts/run-doc-gen.sh
+++ ci/scripts/run-doc-gen.sh
@@ -5,8 +5,8 @@ env > outputs/README-1.0.md
 find .
 if ! grep "** OK **" test-data/test-data-1.0.0.txt;
 then
-    echo "TEST OK" | tee -a outputs/README\-1.0.md
+    echo "TEST OK" | tee -a outputs/README-1.0.0.md
 else
-    echo "TEST FAILURE" | tee -a outputs/README\-1.0.md
+    echo "TEST FAILURE" | tee -a outputs/README-1.0.0.md
     exit 250;
 fi
